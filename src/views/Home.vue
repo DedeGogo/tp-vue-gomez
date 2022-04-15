fdf8a3d4cb7b422cb3067857daa82d5b21569ed7
Andres Gomez <andres@lilo.org>
2022-04-15 16:30:30 +0200

    wip: basic views

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <router-link :to="{ name: 'UserApp' }">User App</router-link>
+    <router-link :to="{ name: 'AdminApp' }">Admin App</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+/* Is this View necesarry?!? If it is, it should do something !!!!
+ */
+export default {
+  name: "Home",
+
+  components: {},
+};
+</script>
---
+++ src/views/admin/AdminApp.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <nav-bar />
+    <nav-drawer />
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+    <app-footer />
+  </v-app>
+</template>
+
+<script>
+import NavDrawer from "../../components/NavDrawer.vue";
+import NavBar from "../../components/NavBar.vue";
+import AppFooter from "../../components/AppFooter.vue";
+
+export default {
+  components: { NavDrawer, NavBar, AppFooter },
+  name: "UserApp",
+  data: () => ({
+    drawer: true,
+  }),
+};
+</script>
+
+<style>
+router-link {
+  margin: 5px;
+}
+</style>
